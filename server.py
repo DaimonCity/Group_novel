b7a2d05c88069b44984972fb5687d25c1485b835
Dmitrii <dsamojlov04@gmail.com>
2025-05-06 23:17:20 +0700

    Добавлен index.css файл к index.html

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///novel.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 # Модель для главы
 class Chapter(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,26 +16,30 @@ class Chapter(db.Model):
     votes = db.Column(db.Integer, default=0)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
+
 # Создаем базу данных
 with app.app_context():
     db.create_all()
+
 
 @app.route('/')
 def index():
     chapters = Chapter.query.order_by(Chapter.date.desc()).all()
     return render_template_string(open('index.html', 'r', encoding='utf-8').read(), chapters=chapters)
 
+
 @app.route('/add_chapter', methods=['POST'])
 def add_chapter():
     content = request.form['content']
     author = request.form['author']
-    
+
     if content and author:
         new_chapter = Chapter(content=content, author=author)
         db.session.add(new_chapter)
         db.session.commit()
-    
+
     return redirect(url_for('index'))
+
 
 @app.route('/vote/<int:chapter_id>')
 def vote(chapter_id):
@@ -42,6 +47,7 @@ def vote(chapter_id):
     chapter.votes += 1
     db.session.commit()
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
