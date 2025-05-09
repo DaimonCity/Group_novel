687990d07361ba7ebbeb92fdc1ca3461fca6b672
Ordo_max <gorobec8800@gmail.com>
2025-05-09 13:53:13 +0300

    редактор перенесен на отдельный адресс

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import flask_login
+import sqlalchemy
 from flask import Flask, render_template, request, redirect, url_for
 from bs4 import BeautifulSoup
 from flask_login import login_user, login_required, logout_user, UserMixin, LoginManager
@@ -7,6 +8,7 @@ from forms.login import LoginForm
 from data import db_session
 from data.users import User
 from data.chapters import Chapter
+from data.Сontinue_chapters import Сontinue_chapters
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -20,27 +22,41 @@ def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
 
+
 @app.route('/')
 def index():
     db_sess = db_session.create_session()
-    chapters = [char for char in db_sess.query(Chapter).all() if char.status == 0]
+    chapters = [char for char in db_sess.query(Chapter).all() if char.state == 0]
     return render_template('index.html', chapters=chapters)
 
 
 @app.route('/edit')
 def editor():
-    return render_template('docs.html')
+    return render_template('docs2.html')
+
+
+@app.route('/continue_chapter/<int:chapter_id>')
+def continue_chapter(chapter_id):
+    db_sess = db_session.create_session()
+    chapter = db_sess.query(Chapter).get(chapter_id)
+    print(db_sess.query(Сontinue_chapters).get().all())
+    add_chapter()
+    chapter.continues_id.append()
+
+    db_sess.commit()
+    return redirect(url_for('index'))
 
 
 @app.route('/add_chapter', methods=['POST'])
 def add_chapter():
     db_sess = db_session.create_session()
     content = request.form['content']
-    author = flask_login.current_user.name
-    if content and author:
-        new_chapter = Chapter(content=content, author=author)
+    author_id = flask_login.current_user.id
 
+    if content and author_id:
+        new_chapter = Chapter(content=content, author_id=author_id)
         db_sess.add(new_chapter)
+        db_sess.query(Сontinue_chapters).add_column(sqlalchemy.Column(sqlalchemy.Integer))
         db_sess.commit()
 
     return redirect(url_for('index'))
@@ -53,16 +69,6 @@ def vote(chapter_id):
     chapter.votes += 1
     db_sess.commit()
     return redirect(url_for('index'))
-
-@app.route('/continue_chapter/<int:chapter_id>')
-def continue_chapter(chapter_id):
-    db_sess = db_session.create_session()
-    chapter = db_sess.query(Chapter).get(chapter_id)
-    chapter.votes += 1
-    db_sess.commit()
-    return redirect(url_for('index'))
-
-
 
 
 @app.route('/publish_chapter', methods=['POST'])
@@ -80,20 +86,22 @@ def publish_chapter():
         db_sess.commit()
         # Очищаем localStorage после успешной отправки
         return redirect(url_for('editor'))
-
     return "Ошибка: текст или автор не указаны", 400
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def reqister():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
+            return render_template('register.html',
+                                   title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
+            return render_template('register.html',
+                                   title='Регистрация',
                                    form=form,
                                    message="Такой пользователь уже есть")
         user = User(
@@ -120,7 +128,9 @@ def login():
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
-    return render_template('login.html', title='Авторизация', form=form)
+    return render_template('login.html',
+                           title='Авторизация',
+                           form=form)
 
 
 @app.route('/logout')
