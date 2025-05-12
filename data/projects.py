33607ab03f7925b305d33c030fd92dcc3ec97444
Ordo_max <gorobec8800@gmail.com>
2025-05-12 12:16:29 +0300

    Добавление глав и их редактирование Сделано

--- data/projects.py
+++ data/projects.py
@@ -7,10 +7,10 @@ from sqlalchemy import Column, Integer, ForeignKey, DateTime, String, Text
 class Project(SqlAlchemyBase, UserMixin):
     __tablename__ = 'projects'
 
-    id = Column(Integer, primary_key=True)
+    # id = Column(Integer, primary_key=True)
     author_id = Column(Integer, ForeignKey("users.id"))
     state = Column(Integer, default=0)  # 0-Private 1-Shown 2-Shown + Edit
     votes = Column(Integer, default=0)
     date = Column(DateTime, default=datetime.utcnow)
     title = Column(Text, nullable=False)
-    chapter_id = Column(Text, ForeignKey("chapters.id"))
+    chapter_id = Column(Integer, ForeignKey("chapters.id"), primary_key=True)
